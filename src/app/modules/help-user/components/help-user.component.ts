acddede247f36de3a80a7d7b9f5f88e366e1e2f8
Eduardo282x <eduardokunmixyeah@gmail.com>
2023-10-26 15:11:01 -0400

    add fotter in helpcomponent

--- src/app/modules/help-user/components/help-user.component.ts
+++ src/app/modules/help-user/components/help-user.component.ts
@@ -25,6 +25,15 @@ export class HelpUserComponent implements OnInit {
     );
   }
 
+  footerData: string[] = [
+    'Diseñado por ACH Systems "Sistemas a tu medida"',
+    'J-40658132-1 ',
+    '2023 ©Todos los derechos reservados ',
+    '¡Siguenos! @achsystems ',
+    '+58 261-732-1543 ',
+    'Zulia Venezuela',
+  ];
+
 
   ngOnInit(): void {
       
